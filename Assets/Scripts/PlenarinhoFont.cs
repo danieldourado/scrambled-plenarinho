b3b8a560f8cf44eb9f2f04da9432eaf76022d77d
Daniel D Fernandes <daniel.dourado@gmail.com>
2020-06-08 16:49:51 -0300

    adiciona controle de de texto

---
+++ Assets/Scripts/PlenarinhoFont.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class PlenarinhoFont : MonoBehaviour
+{
+    [SerializeField] int fontSize = 20; 
+    [SerializeField] int charLimit = 140; 
+    Text text;
+    string originalText;
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        originalText = text.text;
+    }
+    void Update()
+    {
+        if(charLimit < 1) charLimit = 1;
+        if(fontSize < 1) fontSize = 1;
+        
+        text.resizeTextForBestFit = false;
+        text.fontSize = fontSize;
+        text.text = originalText.Substring(0, charLimit);
+    }
+}
